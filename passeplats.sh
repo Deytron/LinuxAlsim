17dc65067636a0baf98da4cfc49a4aee019aa389
Romain Ranaivoson <lemalgache@pop-os.localdomain>
2020-02-28 18:06:24 +0100

    Ca marche ! V1

--- passeplats.sh
+++ passeplats.sh
@@ -1,11 +1,6 @@
 # Script pour l'éval je suis trop chaud
 
-# Boucle pour récupérer les fichiers dans tmp/in
-
-tmpdossierin="/tmp/in"
-tmpdossierout="/tmp/out"
-
-gzip -r $tmpdossierin
-mv $tmpdossierin $tmpdossierout
+gzip -r /tmp/in
+mv /tmp/in/* /tmp/out
 
 
