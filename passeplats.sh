9da79070771928655c9b71e3908090c4b198f9d1
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2020-03-01 22:45:13 +0100

    Version 2 100% working

--- passeplats.sh
+++ passeplats.sh
@@ -1,10 +1,13 @@
 # Script pour l'éval je suis trop chaud
 
-for file in "/tmp/in"; do
-	if [[ $file == *.lock ]]; then
+for file in "/tmp/out"; do
+	if test -f "/tmp/out/running.lock"; then
+		echo "ERREUR 22 : Operation en cours"
 		exit 22
 	else
+		touch running.lock
 		gzip -r /tmp/in
 		mv /tmp/in/* /tmp/out
-
-
+		rm running.lock
+		fi
+done
