238bb3f8416d3f5a53d9933da7de92e3f3a807e4
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2020-03-08 18:50:53 +0100

    Version 3

--- passeplats.sh
+++ passeplats.sh
@@ -1,13 +1,20 @@
 # Script pour l'éval je suis trop chaud
 
-for file in "/tmp/out"; do
-	if test -f "/tmp/out/running.lock"; then
-		echo "ERREUR 22 : Operation en cours"
+for file in "/tmp/"; do
+	if [ ! -d "/tmp/in" ]; then
+		echo "ERREUR 5 : Aucun dossier tmp/in présent. Aucune opération à effectuer."
+		exit 5
+	elif test -f "/tmp/in/running.lock"; then
+		echo "ERREUR 22 : Operation en cours. Veuillez stopper l'opération actuelle."
 		exit 22
-	else
+	elif find /tmp/in -mindepth 1 | read; then
+		mkdir -p /tmp/out
 		touch running.lock
 		gzip -r /tmp/in
-		mv /tmp/in/* /tmp/out
+		mv /tmp/in/* /tmp/out/
 		rm running.lock
-		fi
+        else     
+                echo "ERREUR 7 : Le dossier tmp/in est vide. Aucune opération à effectuer"
+                exit 7
+	fi
 done
